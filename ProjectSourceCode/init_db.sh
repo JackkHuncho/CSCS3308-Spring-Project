75d4f50cb863b6f884490e33ab3a4bebec17adb1
Kendrix Henderson <kendrixhenderson@gmail.com>
2025-04-17 14:04:04 -0600

    ignored certain file

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -7,7 +7,7 @@
 PG_URI="postgresql://postgres1:y0NReyww9H3c8yLuKeLwLOCkYRa1R7DE@dpg-d00j2r1r0fns73ebf860-a.oregon-postgres.render.com/user_db_1rgw"
 
 # Execute each .sql file in the directory
-for file in init_data/*.sql; do
+for file in src/init_data/*.sql; do
     echo "Executing $file..."
     psql $PG_URI -f "$file"
 done
